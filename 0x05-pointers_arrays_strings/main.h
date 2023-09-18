ed16a99a4a73a919ccc772c8b07df96f05384ade
kiey13 <kieyinn@gmail.com>
2023-09-18 15:29:34 +0000

    header file for this project

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98 (int *n);
+void swap_int(int *a, int *b);
+int _stren(char *5);
+void _puts (char *str);
+void print_rev(char *s);
+void rev string(char *s);
+void puts (char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char _strepy(char *dest, char *src);
+
+#endif
